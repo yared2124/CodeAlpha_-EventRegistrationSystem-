5feee125ecafc865464953b98b7b613d9ea133d4
yared aregayehu <yaredbaby21@gmail.com>
2026-07-20 02:15:25 +0300

    updated

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # ------------------------------------------------------------
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'event_db'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
